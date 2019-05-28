a5ef24805fa4ddd0863e75f18694509130946fbd
threeon <ibksyong@gmail.com>
2019-05-28 10:01:41 +0900

    dump 화일 갱신

---
+++ client/src/js/config_test.js
@@ -0,0 +1,10 @@
+/*
+ * 설정
+ */
+
+module.exports = {
+    // base_url: "http://localhost:8031",
+    // base_url: "https://stocknet.koscom.co.kr",
+    // base_url: "http://stocknetmain.koscom.co.kr",
+    base_url: "http://stocknettest.koscom.co.kr",
+}
